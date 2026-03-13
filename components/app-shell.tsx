bb1c6506a2bae759b1ab96776e7a1997d1f694a8
Kauan <reykauan6@gmail.com>
2026-03-13 11:34:32 -0300

    fix: revert app-shell

--- components/app-shell.tsx
+++ components/app-shell.tsx
@@ -10,14 +10,16 @@ export function AppShell({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="flex min-h-screen flex-col">
-      <Header onToggleSidebar={() => setSidebarOpen((v) => !v)} />
-      <AppSidebar
-        open={sidebarOpen}
-        onClose={() => setSidebarOpen(false)}
-      />
-      <div className="flex flex-1 flex-col">
-        <main className="flex-1 p-4 lg:p-8">{children}</main>
-        <Footer />
+      <Header onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+      <div className="flex flex-1">
+        <AppSidebar
+          open={sidebarOpen}
+          onClose={() => setSidebarOpen(false)}
+        />
+        <div className="flex flex-1 flex-col">
+          <main className="flex-1 p-4 lg:p-6">{children}</main>
+          <Footer />
+        </div>
       </div>
     </div>
   )
